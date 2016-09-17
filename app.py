7f1bd3ba8f2d70b1dcac97176475312e447d0c0c
kaizu <jumpei_kaizu@shino.ecei.tohoku.ac.jp>
2016-09-17 16:45:51 +0900

    search

--- app.py
+++ app.py
@@ -31,13 +31,33 @@ def profile(Id):
     prof["address"] = data[2]
     prof["lat"] = data[3]
     prof["lng"] = data[4]
-    #return json.dumps(data)
     return render_template('profile.html',prof=prof)
     
-@app.route('/search')
+@app.route('/search',methods=["POST"])
 def search():
     data=[]
-    return "search"
+    date = request.form["date"]
+    start_hour = request.form["start_hour"]
+    end_hour = request.form["end_hour"]
+    
+    sql = 'select id from schedules where date = {0} and ({1} <= start_hour and {2} >= end_hour);'.format(date, start_hour, end_hour)
+    cur = g.db.execute(sql)
+    result_ids = [record[0] for record in cur.fetchall()]
+    
+    sql = 'select * from profile where id in ('
+    place_holder = '?,'*len(result_ids)
+    sql = sql + place_holder[:-1] + ')'
+    cur = g.db.execute(sql, result_ids)
+    for row in cur.fetchall():
+        prof={}
+        prof["id"] = row[0]
+        prof["name"] = row[1]
+        prof["address"] = row[2]
+        prof["lat"] = row[3]
+        prof["lng"] = row[4]
+        data.append(prof)
+        
+    return json.dumps(data)
 
 @app.before_request
 def before_request():
